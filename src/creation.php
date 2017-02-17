8cf043da3ed1c906405c9b0285221d1bf978a51b
Dave Aditya <adave@adobe.com>
2017-02-16 19:58:22 -0700

    none

--- src/creation.php
+++ src/creation.php
@@ -1,5 +1,39 @@
 <?php
 
+// include stuff
+session_start(); /* Session */
+$con=mysqli_connect("localhost","root","root","pls"); /*Database Connection*/
+
+/*Function to get users data*/
+function get_user_data($con, $user_id){
+$result = mysqli_query($con, "SELECT U.*, P.name FROM tbl_users U LEFT JOIN tbl_user_profile P ON U.user_ID=P.user_id WHERE U.user_id='$user_id' LIMIT 1");
+    if(mysqli_num_rows($result)==1){
+        return mysqli_fetch_assoc($result);
+    }else{
+    	return FALSE;
+    }
+}
+
+function safe_input($con, $data) {
+  return htmlspecialchars(mysqli_real_escape_string($con, trim($data)));
+}
+
+/*Function to set JSON output*/
+function output($Return=array()){
+    /*Set response header*/
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    /*Final JSON response*/
+    exit(json_encode($Return));
+}
+
+// end include stuff
+
+
+if((empty($_SERVER['HTTP_X_REQUESTED_WITH']) or strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') or empty($_POST)){/*Detect AJAX and POST request*/
+  exit("Unauthorized Acces");
+}
+
 /* Check Registration form submitted */
 if(!empty($_POST) && $_POST['Action']=='create_user'){
     /* Define return | here result is used to return user data and error for error message */
